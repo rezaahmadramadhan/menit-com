c12fd1194431e31bc79f2d1c7efe6baa908d4d2d
rezaahmadramadhan <dhronzae@gmail.com>
2025-04-14 21:03:38 +0700

    Get /articles and handling error 500 done

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,4 +1,4 @@
-const { Article } = require("../models")
+const { Article, User} = require("../models")
 
 class articleController {
     static async createArticle(req, res) {
@@ -9,6 +9,23 @@ class articleController {
             if (error.name === "SequelizeValidationError") {
                 return res.status(400).json({ message: error.errors[0].message })
             }
+            res.status(500).json({ message: "Internal Server Error" })
+        }
+    }
+
+    static async getArticle(req, res) {
+        try {
+            const article = await Article.findAll({
+                include: {
+                    model: User,
+                    attributes: {
+                        exclude: ['password']
+                    }
+                }
+            })
+
+            res.status(200).json(article)
+        } catch (error) {
             res.status(500).json({ message: "Internal Server Error" })
         }
     }
--- routers/articles.js
+++ routers/articles.js
@@ -3,6 +3,7 @@ const articleController = require('../controllers/articleController');
 const articles = express.Router()
 
 articles.post('/', articleController.createArticle)
+articles.get('/', articleController.getArticle)
 
 
 module.exports = articles
