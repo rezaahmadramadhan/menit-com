b3cebe1ef2d80cd0d952de4ea86c33a0ed5ebad3
rezaahmadramadhan <dhronzae@gmail.com>
2025-04-14 21:18:27 +0700

    Get /articles/:id status(200) and handling error 404 & 500 done

--- routers/articles.js
+++ routers/articles.js
@@ -4,6 +4,7 @@ const articles = express.Router()
 
 articles.post('/', articleController.createArticle)
 articles.get('/', articleController.getArticle)
+articles.get('/:id', articleController.getArticleById)
 
 
 module.exports = articles
