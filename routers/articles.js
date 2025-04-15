44a618ead0e4969f78a42789eaf9fc379723eb4b
rezaahmadramadhan <dhronzae@gmail.com>
2025-04-16 01:24:07 +0700

    Install package multer and setting up

--- routers/articles.js
+++ routers/articles.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const ArticleController = require('../controllers/ArticleController');
 const { authAdmin } = require('../middlewares/authorization');
+const multer = require('multer')
 const articles = express.Router()
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 articles.post('/', ArticleController.createArticle)
 articles.get('/', ArticleController.getArticle)
 articles.get('/:id', ArticleController.getArticleById)
 articles.put('/:id', authAdmin, ArticleController.updateArticleById)
+articles.patch('/:id/img-url', authAdmin, ArticleController.updateArticleById)
 articles.delete('/:id', authAdmin, ArticleController.deleteArticleById)
 
 
