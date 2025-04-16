08a5355d4876ea310e2b6166160e50a978659ec2
rezaahmadramadhan <dhronzae@gmail.com>
2025-04-16 12:46:32 +0700

    Install package supertest in devDependencies, make dir bin/www & move app listen into it

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const express = require('express');
 const errorHandler = require('./middlewares/errorHandler');
 const app = express()
-const port = 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
@@ -12,6 +11,4 @@ app.use('/', require("./routers"))
 
 app.use(errorHandler)
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+module.exports = app
