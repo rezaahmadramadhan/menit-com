ed3f96ea111a35a5cd1d324b053d828de1955eb6
rezaahmadramadhan <dhronzae@gmail.com>
2025-04-17 16:58:07 +0700

    Try Deployment

--- __tests__/public.test.js
+++ __tests__/public.test.js
@@ -7,29 +7,12 @@ const app = require("../app");
 const categories = require("../data/category.json")
 const articles = require("../data/article.json")
 
-let userAdmin
-let userStaff
-let accessTokenAdmin
-let accessTokenStaff
-
 beforeAll(async () => { 
-    userAdmin = await User.create({email: "testAdmin@gmail.com", role: "Admin", password: "admin"})
-    accessTokenAdmin = signToken({id: userAdmin.id})
-
-    userStaff = await User.create({email: "testStaff@gmail.com", role: "Staff", password: "rahasia"})
-    accessTokenStaff = signToken({id: userStaff.id})
-
     await Category.bulkCreate(categories)
     await Article.bulkCreate(articles)
 })
 
 afterAll(async () => {
-    await User.destroy({
-        truncate: true,
-        restartIdentity: true,
-        cascade: true
-    })    
-
     await Category.destroy({
         truncate: true,
         restartIdentity: true,
@@ -89,5 +72,40 @@ describe('GET /pub/articles', () => {
         expect(response.body.data[0]).toHaveProperty("authorId", expect.any(Number))
         expect(response.body.data[0]).toHaveProperty("createdAt", expect.any(String))
         expect(response.body.data[0]).toHaveProperty("updatedAt", expect.any(String))
+    })
+
+    test('GET /pub/articles should SUCCESS get public articles, with length data per page', async () => {
+        const response = await request(app)
+                        .get('/pub/articles?page=3&limit=10')
+        
+        expect(response.status).toBe(200)
+        expect(response.body.data).toBeInstanceOf(Array)
+        expect(response.body).toHaveProperty("page", 3)
+        expect(response.body).toHaveProperty("pageData", 10)
+        expect(response.body.data[0]).toHaveProperty("title", expect.any(String))
+        expect(response.body.data[0]).toHaveProperty("content", expect.any(String))
+        expect(response.body.data[0]).toHaveProperty("imgUrl", expect.any(String))
+        expect(response.body.data[0]).toHaveProperty("categoryId", expect.any(Number))
+        expect(response.body.data[0]).toHaveProperty("authorId", expect.any(Number))
+        expect(response.body.data[0]).toHaveProperty("createdAt", expect.any(String))
+        expect(response.body.data[0]).toHaveProperty("updatedAt", expect.any(String))
+    })
+})
+
+describe('GET /pub/articles/:id', () => {
+    test('GET /pub/articles/:id should SUCCESS get public detail articles', async () => {
+        const response = await request(app)
+                        .get('/pub/articles/1')
+        
+        expect(response.status).toBe(200)
+        expect(response.body).toBeInstanceOf(Object)
+        expect(response.body).toHaveProperty("id", 1)
+        expect(response.body).toHaveProperty("title", expect.any(String))
+        expect(response.body).toHaveProperty("content", expect.any(String))
+        expect(response.body).toHaveProperty("imgUrl", expect.any(String))
+        expect(response.body).toHaveProperty("categoryId", expect.any(Number))
+        expect(response.body).toHaveProperty("authorId", expect.any(Number))
+        expect(response.body).toHaveProperty("createdAt", expect.any(String))
+        expect(response.body).toHaveProperty("updatedAt", expect.any(String))
     })
 })
