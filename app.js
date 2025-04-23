7e35f6c73f815f423b4ad9b2271c77ff426c748f
rezaahmadramadhan <dhronzae@gmail.com>
2025-04-23 18:00:15 +0700

    Update cors

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const errorHandler = require('./middlewares/errorHandler');
 const app = express()
 const cors = require('cors')
 
+app.use(cors)
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 app.use('/', require("./routers"))
 
-app.use(cors)
 app.use(errorHandler)
 
 module.exports = app
