90b7fe570bc2b00b5447d88615762a35885c8bea
rezaahmadramadhan <dhronzae@gmail.com>
2025-04-23 10:09:10 +0700

    Update use cors

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const errorHandler = require('./middlewares/errorHandler');
 const app = express()
+const cors = require('cors')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 app.use('/', require("./routers"))
 
+app.use(cors)
 app.use(errorHandler)
 
 module.exports = app
